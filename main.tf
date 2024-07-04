7209141758b5077e1f474ec6dca89e495f9e517c
manonmanigithubacc <tmanonmane@gmail.com>
2024-07-04 11:21:16 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -197,8 +197,10 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
 module "s3_bucket" {
-  source              = "app.terraform.io/testorghcp/s3-bucket/aws"
+  source = "terraform-aws-modules/s3-bucket/aws" # Example for a registry module
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
+
